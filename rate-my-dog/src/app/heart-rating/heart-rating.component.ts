2da6795d251e0510b4738583ee9eae244fb19bc3
Daniel <daniel@Daniels-MacBook-Pro.local>
2023-01-23 00:31:56 -0500

    Add heart ratings

--- rate-my-dog/src/app/heart-rating/heart-rating.component.ts
+++ rate-my-dog/src/app/heart-rating/heart-rating.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./heart-rating.component.css']
 })
 export class HeartRatingComponent {
+  public hearts: boolean[] = Array(5).fill(true);
 
+  public rate(rating: number) {
+    console.log('rating', rating);
+    this.hearts = this.hearts.map((_, i) => rating > i);
+    console.log('hearts', this.hearts);
+  }
 }
